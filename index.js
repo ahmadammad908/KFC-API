5dfe68510f1bed690ee29622425e39df989a81bf
ahmadammad <ahmadammad789@gmail.com>
2023-09-27 20:08:12 +0500

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
 
 const apiData = require("./data.json");
 app.get("/", (req, res) => {
-  res.send("Hello Iam live");
+  res.send("Hello I am live");
 });
-app.get("/service", (req, res) => {
-  res.send(apiData);
+
+// Add a route to get unique categories from the data
+app.get("/categories", (req, res) => {
+  const categories = [...new Set(apiData.map((item) => item.category))];
+  res.json(categories);
 });
+
+app.get("/products", (req, res) => {
+  res.json(apiData);
+});
+
 app.listen(port, () => {
-  console.log("I am Live  ");
+  console.log("I am Live");
 });
